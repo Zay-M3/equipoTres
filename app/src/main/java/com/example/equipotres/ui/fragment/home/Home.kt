64a0afbc46d6b754a921aa10d10fa9e33b3aa19e
CamTabUnivalle <juan.camilo.taborda@correounivalle.edu.co>
2025-11-10 23:21:28 -0500

    Agregando la ruta del login cuando hace logout

--- app/src/main/java/com/example/equipotres/ui/fragment/home/Home.kt
+++ app/src/main/java/com/example/equipotres/ui/fragment/home/Home.kt
@@ -1,9 +1,9 @@
 package com.example.equipotres.ui.fragment.home
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -48,6 +48,7 @@ class Home : Fragment(R.layout.fragment_home) {
     }
 
     //Muestra los ítems en el RecyclerView.
+    @SuppressLint("NotifyDataSetChanged")
     private fun observerListInventory(){
         inventoryViewModel.getListInventory()
         inventoryViewModel.listInventory.observe(viewLifecycleOwner){ listInventory ->
